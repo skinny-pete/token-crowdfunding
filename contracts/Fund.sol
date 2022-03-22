93afde1340a814c713bb682cc2be4764ee0bd60b
Peter W <pwhitby8@gmail.com>
2022-03-22 12:33:41 +0000

    Clean up, added closed()

--- contracts/Fund.sol
+++ contracts/Fund.sol
@@ -8,9 +8,14 @@ import "@openzeppelin/contracts/interfaces/IERC20.sol";
 contract Fund is ERC20 {
     address public projectDeveloper;
     address private owner;
+    address feeTo;
+
+    uint feeNumerator = 3;
+    uint feeDenominator = 100;
 
     address public currency;
-    uint256[] public caps;
+    uint256[] public capsAmount;
+    uint256[] public capsTime;
 
     mapping(address => bool) whitelist;
 
@@ -25,13 +30,17 @@ contract Fund is ERC20 {
     event Withdrawl(uint256 amount);
 
     constructor(
+        address _feeTo,
         address _projectDeveloper,
         address _currency,
-        uint256[] memory _caps
+        uint256[] memory _capsAmount,
+        uint[] memory _capsTime
     ) ERC20("Fund", "FND") {
+        feeTo = _feeTo;
         projectDeveloper = _projectDeveloper;
         currency = _currency;
-        caps = _caps;
+        capsAmount = _capsAmount;
+        capsTime = _capsTime;
         totalContributions = 0;
         owner = msg.sender;
     }
@@ -46,25 +55,39 @@ contract Fund is ERC20 {
         _;
     }
 
+    function closed() public view returns (bool) {
+        return
+            (block.timestamp >= capsTime[currentCap] &&
+                totalContributions <= capsAmount[currentCap]) ||
+            totalContributions >= capsAmount[capsAmount.length - 1];
+    }
+
+    function applyFee(uint amount) internal view returns (uint) {
+        return (amount * feeNumerator) / feeDenominator;
+    }
+
     function deposit(uint256 amount) public whitelisted returns (bool) {
+        require(!closed(), "This project is closed");
         require(
             IERC20(currency).transferFrom(msg.sender, address(this), amount)
         );
-        require(
-            totalContributions < caps[caps.length - 1],
-            "Already at max cap"
-        );
-        if (currentCap < caps.length-1) {
+        if (currentCap < capsAmount.length - 1) {
             require(
-                amount <= caps[currentCap+1] - totalContributions,
+                amount <= capsAmount[currentCap + 1] - totalContributions,
                 "Deposit too large, please make two transactions"
             );
         }
 
-        if(currentCap == caps.length-1) {
-            require(amount <= caps[caps.length-1] - totalContributions, 
-            "This deposit would exceed max cap");
+        if (currentCap == capsAmount.length - 1) {
+            require(
+                amount <=
+                    capsAmount[capsAmount.length - 1] - totalContributions,
+                "This deposit would exceed max cap"
+            );
         }
+
+        uint feeCollected = applyFee(amount);
+        IERC20(currency).transfer(feeTo, feeCollected);
 
         uint256 toMint;
         if (totalContributions == 0) {
@@ -75,13 +98,15 @@ contract Fund is ERC20 {
         }
 
         totalContributions += amount;
-        if (totalContributions >= caps[currentCap]) {
-            currentCap = currentCap < caps.length-1 ? currentCap+1 : currentCap;
+        if (totalContributions >= capsAmount[currentCap]) {
+            currentCap = currentCap < capsAmount.length - 1
+                ? currentCap + 1
+                : currentCap;
         }
 
         _mint(msg.sender, toMint);
 
-        emit Deposit(amount, msg.sender);
+        emit Deposit(amount, msg.sender); //TODO emit event with full amount, or amount minus fee?
 
         return true;
     }
@@ -92,16 +117,20 @@ contract Fund is ERC20 {
             "Only project developer may withdraw"
         );
         require(currentCap > 0, "Cannot withdraw until secondary cap reached");
-        
-        if(currentCap == caps.length-1) { //If have reached last cap - release all remaining funds
+
+        if (currentCap == capsAmount.length - 1) {
+            //If have reached last cap - release all remaining funds
             uint balance = IERC20(currency).balanceOf(address(this));
             IERC20(currency).transfer(projectDeveloper, balance);
+        } else {
+            //Otherwise transfer the last complete cap
+            IERC20(currency).transfer(
+                projectDeveloper,
+                capsAmount[currentCap - 1] - withdrawn
+            );
+            withdrawn += capsAmount[currentCap - 1] - withdrawn;
         }
-        else { //Otherwise transfer the last complete cap
-            IERC20(currency).transfer(projectDeveloper, caps[currentCap-1] - withdrawn);
-            withdrawn += caps[currentCap - 1] - withdrawn;
-        }
-        
+
         return true;
     }
 
@@ -109,5 +138,12 @@ contract Fund is ERC20 {
         whitelist[toApprove] = true;
     }
 
-    
+    function setFeeTo(address newFeeTo) public onlyOwner {
+        feeTo = newFeeTo;
+    }
+
+    function setFee(uint numerator, uint denominator) public onlyOwner {
+        feeNumerator = numerator;
+        feeDenominator = denominator;
+    }
 }
