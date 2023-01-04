e35f9e9980557122419e3ea0a7c5df0ef5aab6f2
Peter W <pwhitby8@gmail.com>
2023-01-04 14:56:02 +0000

    initial

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,8 +10,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 
 
 
-const ROPSTEN_PRIVATE_KEY = "8d9b7db94ffca8090775c3bcc600a7b37cbe97eeba2add683118390f0a5ce9d6"
-const MUMBAI_PRIVATE_KEY = "797c232d058a2d275d4fdcb688697bba503debe9a31caa9a3523ed34f46a431d"
+const ROPSTEN_PRIVATE_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+const MUMBAI_PRIVATE_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -20,7 +20,7 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     ropsten: {
-      url: "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      url: "https://ropsten.infura.io/v3/xxxxxxxxxxxxxxxxxxxx",
       accounts: [ROPSTEN_PRIVATE_KEY]
     },
     mumbai: {
